22dbf68c803cd87b5d48676973362dda4030d9a7
taslimy <taslimer@gmail.com>
2019-04-22 15:44:55 -0400

    Added my branch and starting

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Header.css';
+
+function HeaderComponents() {
+  return (
+<div className="header-container">
+testing if this works.
+</div>
+  );
+}
+
+
+
+
+export default HeaderComponents;
